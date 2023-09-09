a8dcd033cfa62b0377cb4dea07c4c862bf39fb5b
tyrantsMedic <hamzaofe786@hotmail.com>
2023-09-09 09:10:08 +0500

    changed port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 80;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
